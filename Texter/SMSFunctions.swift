b66fdb1c6ec2baf3de43ae4d880cca7b0ac6fc54
Daniel ILLESCAS ROMERO <Daniel@daniel.home>
2016-07-30 23:20:14 +0200

    Reduced code

--- Texter/SMSFunctions.swift
+++ Texter/SMSFunctions.swift
@@ -1,32 +1,20 @@
 import Foundation
 
-// Sends SMS via HTTP
+// Send SMS via HTTP
 func sendTextMessage (message: String, number: String) {
 	
 	// Clockwork SMS API - https://www.clockworksms.com
-	let APIkey = "YOUR_API_HERE!"
+	let APIkey = "YOUR_API_KEY_HERE!!"
 	
 	// Request example: https://api.clockworksms.com/http/send.aspx?key=KEY&to=441234567890&content=Hello+World
 	var clockWorkNSURLString = "https://api.clockworksms.com/http/send.aspx?"
 		+ "key=\(APIkey)"
 		+ "&to=\(number)"
 		+ "&content=\(message)"
+	
 	clockWorkNSURLString = clockWorkNSURLString.stringByReplacingOccurrencesOfString(" ", withString: "+")
 	
-	let clockWorkSMSURL = NSURL(string: clockWorkNSURLString)
-	
-	if clockWorkSMSURL != nil {
-		
-		let responseData = NSData(contentsOfURL: clockWorkSMSURL!)
-		
-		if responseData != nil {
-			
-			// Converts the NSData object into a String object
-			let responseString = String(data: responseData!, encoding: NSUTF8StringEncoding)
-			
-			if responseString != nil {
-				print(responseString!)
-			}
-		}
+	if let clockWorkSMSURL = NSURL(string: clockWorkNSURLString), responseData = NSData(contentsOfURL: clockWorkSMSURL) {
+		print(String(data: responseData, encoding: NSUTF8StringEncoding))
 	}
 }
--- Texter/main.swift
+++ Texter/main.swift
@@ -1,13 +1,9 @@
 import Foundation
 
 print("What would you like to text?")
-
-// Collects the message from the user
 guard let userTextMessage = readLine(stripNewline: true) else { exit(0) }
 
 print("Which phone number would you like to send this message to?")
-
-// Collects the number from the user
 guard let toNumber = readLine(stripNewline: true) else { exit(0) }
 
 sendTextMessage(userTextMessage, number: toNumber)
